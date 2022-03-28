880dc56285c50a38b7c89fe2aaeffd7d13248787
Rahulinnu <101334027+Rahulinnu@users.noreply.github.com>
2022-03-29 01:52:03 +0530

    UC1 added

---
+++ UC1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace LineComputation
+{
+    public class UC1
+    {
+        public void UsCa1()
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+            double x1, y1, x2, y2, lineLength;
+
+            Console.WriteLine("Enter first point X coordinate x1: ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter first point Y coordinate y1: ");
+            y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point X coordinate x2: ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second point Y coordinate y2: ");
+            y2 = Convert.ToDouble(Console.ReadLine());
+
+            double powerx = Math.Pow((x2 - x1), 2);
+            double powery = Math.Pow((y2 - y1), 2);
+
+            lineLength = Math.Sqrt((powerx + powery));
+
+            Console.WriteLine($"Length of line using two points ({x1},{y1}) and ({x2},{y2}) is : {lineLength}");
+
+            Console.ReadLine();
+        }
+    }
+}
